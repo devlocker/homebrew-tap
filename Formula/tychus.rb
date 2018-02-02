5bca41b0f64427ef92c3d4da783cc6739f3cf03b
Patrick Koperwas <patrick@devlocker.io>
2018-02-01 16:41:58 -0800

    tychus version v0.5.0

--- Formula/tychus.rb
+++ Formula/tychus.rb
@@ -1,9 +1,9 @@
 class Tychus < Formula
   desc "Command line utility to live-reload your application."
   homepage "https://github.com/devlocker/tychus"
-  url "https://github.com/devlocker/tychus/releases/download/v0.4.0/tychus_0.4.0_darwin_amd64.tar.gz"
-  version "0.4.0"
-  sha256 "cfa2adaf3ce4c9366004d6081298060eaff0924bb463e75c1fe6bdf8746074c2"
+  url "https://github.com/devlocker/tychus/releases/download/v0.5.0/tychus_0.5.0_darwin_amd64.tar.gz"
+  version "0.5.0"
+  sha256 "f593bdbd591e5a59113f729edc473f5abda993411e406a9a8af9c2c74c54fb7a"
 
   def install
     bin.install "tychus"
